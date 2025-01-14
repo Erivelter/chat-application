a65ccb42c7c5ca1e1ebeeeccf954ebc2afbc0a73
Erivelter <eriveltervilasboas@hotmail.com>
2025-01-13 21:37:36 -0300

    Create_folder_server_and_client

---
+++ server/index.js
@@ -0,0 +1,10 @@
+//Esse é o arquivo que você deve rodar, entendeu!
+const { app, server } = require('.'); 
+require('dotenv').config();
+
+const PORT = 4600
+
+server.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
+
